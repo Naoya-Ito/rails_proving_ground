c87b9f3ec4649715441700273cb34707b73b988c
n_ito <n_ito@okwave.co.jp>
2014-07-15 16:32:27 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsProvingGround::Application.config.secret_key_base = 'b5ea0d149bfeafcfe2c0cc1bc9ae0deffa25eba9406a2f489f850ae0ddd6e0e6c21d056e8c3b08c1c08a2c8353a7bfce42e218550555ab3a9e09a8dbbefa69cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsProvingGround::Application.config.session_store :cookie_store, key: '_rails_proving_ground_session'

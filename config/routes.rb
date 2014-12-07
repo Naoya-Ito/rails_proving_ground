b2e0a08c2f57e6939d281d6d76cb7ed5aab606ce
itau <naoyaito@itounaoya-no-MacBook-Pro.local>
2014-12-07 20:48:34 +0900

    勉強会資料

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 RailsProvingGround::Application.routes.draw do
 
-  root :to => 'roots#index'
-
-  get "bunseki" => "roots#bunseki"
+  root          to: "roots#index"
+  get :bunseki, to: "roots#bunseki", as: "bunseki"
+  get :study,   to: "roots#study",   as: "study"
+  get :work,    to: "roots#work",    as: "work"
 
 end

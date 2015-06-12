e6492681446d7b8f419023896b1d8d753f2c7c6c
itau <naoyaito@itounaoya-no-MacBook-Pro.local>
2015-06-12 15:03:07 +0900

    設定変更

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,17 @@ module RailsProvingGround
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+=begin
+    config.autoload_paths += %W(
+          #{ config.root }/app/controllers/concerns
+          #{ config.root }/app/mailers/concerns
+          #{ config.root }/app/models/concerns
+          #{ config.root }/lib
+    )
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += [Rails.root.join('app', 'models','master')]
+    config.i18n.default_locale = 'ja'
+=end
     # compile
     config.assets.precompile += %w( impress.js impress.css )
 
@@ -40,13 +51,29 @@ module RailsProvingGround
       end
     end
 
+    vendor_path = File.join(Rails.root, "vendor/assets/")
+    Find.find(vendor_path).each do |f|
+      if f.match(/^(.*)\.css/) || f.match(/^(.*)\.js/)
+        config.assets.precompile << $1.sub(vendor_path, "")
+      end
+    end
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.generators do |g|
+      g.template_engine :haml
       g.helper false
       g.stylesheets false
       g.javascripts false
+      g.test_framework = "rspec"
+      g.controller_specs = false
     end
   end
+
+  # routing を外部ファイルに分割する
+  #config.paths["config/routes"] << "config/routes/hoge.rb"
+  #config.paths["config/routes"] << "config/routes/moge.rb"
 end
